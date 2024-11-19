7bbed7d5181c23927b96f76cc789bd7d53fe5be1
Jek John Tan <jetan@u82z01s03.42kl.edu.my>
2024-11-19 17:59:39 +0800

    add struct

--- philo.h
+++ philo.h
@@ -3,6 +3,34 @@
 
 #include <stdio.h>
 
+typedef struct s_philo
+{
+	int	num_of_philo;
+	size_t	time_to_die;
+	size_t	time_to_eat;
+	size_t	time_to_sleep;
+	int	num_of_times_to_eat;
+	int	id;
+	int	eating;
+	int	meals_eaten;
+	size_t	last_meal;
+	size_t	start_time;
+	int	*dead;
+	pthread_t	thread;
+	pthread_mutex_t	*r_fork;
+	pthread_mutex_t	*l_fork;
+	pthread_mutex_t	*write_lock;
+	pthread_mutex_t	*dead_lock;
+	pthread_mutex_t	*meal_lock;
+}	t_philo;
 
+typedef struct s_program
+{
+	int	dead_flag;
+	pthread_mutex_t	dead_lock;
+	pthread_mutex_t	meal_lock;
+	pthread_mutex_t	write_lock;
+	t_philo	*philos;
+}	t_program;
 
 #endif
